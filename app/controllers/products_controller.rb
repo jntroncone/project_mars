1f4bf499206d333fd74951997f3651c4e053f8b3
Alec Williams <alecwilliams90121@gmail.com>
2022-03-18 17:10:39 -0500

    Added a add to cart function for each product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+   before_action :initialize_session
+   before_action :increment_visit_count, only: %i[index product]
+   before_action :load_cart
     def show
         @product = Product.find(params[:id])
     end
@@ -6,5 +9,24 @@ class ProductsController < ApplicationController
     def index
         @products = Product.all
         render :index
+    end
+    def add_to_cart
+        id= params[:id].to_i
+        session[:cart] << id unless session[:cart].include?(id)
+        redirect_to root_path
+    end
+    private
+    def initialize_session
+        session[:visit_count] ||=0
+        session[:cart]||=[]
+    end
+
+    def increment_visit_count
+        session[:visit_count] +=1
+        @visit_count = session[:visit_count]
+    end
+
+    def load_cart
+        @cart=Product.find(session[:cart])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   #end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
+  get 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'find_product'
+  post 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
