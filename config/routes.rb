7e7de1c8ce17deaba4718c9dcdeb2d5ff284d1a4
Julie Troncone <jntroncone@gmail.com>
2022-03-22 12:38:19 -0500

    Product CRUD
    CRUD resources for product model

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  get 'products', to: 'products#index', as: 'products'
+  get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
+  post 'products', to: 'products#create'
+  get 'products/new', to: 'products#new', as: 'new_product'
+  get 'products/:id', to: 'products#show', as: 'product'
+  patch 'products/:id', to: 'products#update'
+  delete 'products/:id', to: 'products#destroy'
+
 end
