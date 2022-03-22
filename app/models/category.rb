785f6242eb05bbcdd57cb7e1345c37b3e5060ffd
ttubbs1 <jacksknightemail@gmail.com>
2022-03-22 14:55:00 -0500

    as;jldfk

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@
 # Table name: categories
 #
 #  id         :bigint           not null, primary key
-#  name       :string           not null
+#  name       :string           default(""), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_212049) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
