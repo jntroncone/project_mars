0b619ba91ecdec5e51d35a8d791e731140e21760
Alec Williams <alecwilliams90121@gmail.com>
2022-04-06 13:45:58 -0500

    Deleted duplicate user

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,11 +21,3 @@ item2 = Product.create!(
     description: 'sew em up'
 
 )
-user1 = User.create!(
-  email: "bob@email.com",
-  password: "password",
-  username: "Bob",
-  phone_number: "9019999999",
-  role: "Sales Rep"
-
-)
